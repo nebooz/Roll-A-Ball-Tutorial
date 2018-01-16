93638c39975c56715b24e39258cf27963816eba6
Maximiliano Diaz <nebooz@gmail.com>
2018-01-15 22:31:54 -0800

    Init Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        bool resetPosition = Input.GetKeyDown(KeyCode.R);
+
+        if (resetPosition)
+        {
+            transform.position = new Vector3(0.0f, 0.5f, 0.0f);
+        }
+
+        print(rb.velocity);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
